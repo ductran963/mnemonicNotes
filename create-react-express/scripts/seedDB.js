3b0bcab0da20a17c416a984554089a07e5115137
Louis Alonzo <louis.a@live.com>
2020-03-03 18:53:20 -0800

    modified file to create seeds for database

--- create-react-express/scripts/seedDB.js
+++ create-react-express/scripts/seedDB.js
@@ -7,3 +7,32 @@ mongoose.connect(
   process.env.MONGODB_URI ||
   "mongodb://localhost/reactreadinglist"
 );
+
+const bookSeed = [
+  {
+    title: "Lord of the Flies",
+    author: "William Golding",
+    synopsis:
+      "The tale of a party of shipwrecked schoolboys, marooned on a coral island, who at first enjoy the freedom of the situation but soon divide into fearsome gangs which turn the paradise island into a nightmare of panic and death.",
+    date: new Date(Date.now())
+  },
+  {
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    synopsis:
+      "The Catcher in the Rye is a 1951 novel by J. D. Salinger. A controversial novel originally published for adults, it has since become popular with adolescent readers for its themes of teenage angst and alienation.",
+    date: new Date(Date.now())
+  },
+];
+
+db.Book
+  .remove({})
+  .then(() => db.Book.collection.insertMany(bookSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
