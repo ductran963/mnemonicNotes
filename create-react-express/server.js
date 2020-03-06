135a7da80d340c86ca7575eda8bb4400994de728
Louis Alonzo <louis.a@live.com>
2020-03-05 19:17:43 -0800

    restored original settings

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -17,7 +17,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist"
+  process.env.MONGODB_URI || "mongodb://localhost:/reactclientlist"
 );
 
 // Send every request to the React app
