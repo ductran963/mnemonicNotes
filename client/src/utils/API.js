aa6268f74240485f535c4b972758d53d6ca19273
Louis Alonzo <louis.a@live.com>
2020-03-05 21:41:59 -0800

    added axios calls for post, put, delete

---
+++ client/src/utils/API.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export default {
+  // Gets all books
+  getClients: function() {
+    return axios.get("/api/clients");
+  },
+  // Gets the book with the given id
+  getClient: function(id) {
+    return axios.get("/api/clients/" + id);
+  },
+  // Updates the client with the given id
+  updateClient: function(id) {
+    return axios.put("/api/clients/" + id);
+  },
+  // Deletes the book with the given id
+  deleteClient: function(id) {
+    return axios.delete("/api/clients/" + id);
+  },
+  // Saves a book to the database
+  saveClient: function(clientData) {
+    return axios.post("/api/clients", clientData);
+  }
+};
