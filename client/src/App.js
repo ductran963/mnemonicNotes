1725c072392d99001141cc7a11ab02ac8dd2ae99
Duc T <ductran963@gmail.com>
2020-03-10 19:10:09 -0700

    saved changes

--- client/src/App.js
+++ client/src/App.js
@@ -53,115 +53,99 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-class App extends Component {
-  render() {
-    return (
-      <Typography variant="body2" color="textSecondary" align="center">
-        {'Copyright © '}
-        <Link color="inherit" href="https://material-ui.com/">
-          Your Website
-        </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
-      </Typography>
-    );
-  }
-}
-  
-  
-  
-  export default function Album() {
-    const classes = useStyles();
-    const preventDefault = event => event.preventDefault();
-  
-    return (
-      <React.Fragment>
-        <CssBaseline />
-        <AppBar position="relative">
-          <Toolbar>
-            <HomeIcon className={classes.icon} />
-            {/* <a href="https://www.w3schools.com">Visit W3Schools.com!</a> */}
-            <Typography variant="h6" color="inherit" noWrap>
-              
-              <Link href="#" onClick={preventDefault} color="inherit">
-                {'Home'}
-              </Link>
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <main>
-          {/* Hero unit */}
-          <div className={classes.heroContent}>
-            <Container maxWidth="sm">
-              <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-                Mnemonic Notes
+
+export default function Album() {
+  const classes = useStyles();
+  const preventDefault = event => event.preventDefault();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <HomeIcon className={classes.icon} />
+          {/* <a href="https://www.w3schools.com">Visit W3Schools.com!</a> */}
+          <Typography variant="h6" color="inherit" noWrap>
+
+            <Link href="#" onClick={preventDefault} color="inherit">
+              {'Home'}
+            </Link>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              Mnemonic Notes
               </Typography>
-              <Typography variant="h5" align="center" color="textSecondary" paragraph>
-                "I'm really good with names"
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              "I'm really good with names"
               </Typography>
-              <div className={classes.heroButtons}>
-                <Grid container spacing={2} justify="center">
-                  <Grid item>
-                    <Button variant="contained" color="primary">
-                      Add Client
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    Add Client
                     </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button variant="outlined" color="primary">
-                      Add Friend
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Add Friend
                     </Button>
-                  </Grid>
                 </Grid>
-              </div>
-            </Container>
-          </div>
-          <Container className={classes.cardGrid} maxWidth="md">
-            {/* End hero unit */}
-            <Grid container spacing={4}>
-              {cards.map(card => (
-                <Grid item key={card} xs={12} sm={6} md={4}>
-                  <Card className={classes.card}>
-                    <CardMedia
-                      className={classes.cardMedia}
-                      image="https://source.unsplash.com/random"
-                      title="Image title"
-                    />
-                    <CardContent className={classes.cardContent}>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        Card Notes
-                      </Typography>
-                      <Typography>
-                        This is a media card. You can use this section to describe the content.
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <Button size="small" color="primary">
-                        View
-                      </Button>
-                      <Button size="small" color="primary">
-                        Edit
-                      </Button>
-                    </CardActions>
-                  </Card>
-                </Grid>
-              ))}
-            </Grid>
+              </Grid>
+            </div>
           </Container>
-        </main>
-        {/* Footer */}
-        <footer className={classes.footer}>
-          <Typography variant="h6" align="center" gutterBottom>
-            Footer
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map(card => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Card Notes
+                      </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the content.
+                      </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                      </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                      </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
           </Typography>
-          <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-            Add links or copy right info
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Add links or copyrighted info
           </Typography>
-          
-        </footer>
-        {/* End footer */}
-      </React.Fragment>
-    );
-  }
+
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+  );
+}
 
 
 
